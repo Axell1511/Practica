cc698dc638e2018479d7cb54eb0017eae45502f0
Axell1511 <axellsk1511@gmail.com>
2024-04-18 10:25:58 -0400

    7

--- MyApp.js
+++ MyApp.js
@@ -14,7 +14,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //
-app.use( express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
     //Acceso a Home
